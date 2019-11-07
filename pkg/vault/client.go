870109dc2273862477ac322d56feb878b9b92f66
Paul Hanyzewski <hanyzewski@gmail.com>
2019-11-07 10:14:12 -0700

    Rename c to vc to reflect that it is a vault client.

--- pkg/vault/client.go
+++ pkg/vault/client.go
@@ -28,40 +28,40 @@ func newVaultClient() *vaultClient {
 	return c
 }
 
-func (c *vaultClient) loadVaultEnvironment() error {
+func (vc *vaultClient) loadVaultEnvironment() error {
 
 	traceEnabledString := getEnv("TRACE_ENABLED", "false")
-	c.traceEnabled, _ = strconv.ParseBool(traceEnabledString)
+	vc.traceEnabled, _ = strconv.ParseBool(traceEnabledString)
 
-	if c.traceEnabled {
+	if vc.traceEnabled {
 		var span *trace.Span
-		c.ctx, span = trace.StartSpan(c.ctx, fmt.Sprintf("%s/loadVaultEnvironment", c.tracePrefix))
+		vc.ctx, span = trace.StartSpan(vc.ctx, fmt.Sprintf("%s/loadVaultEnvironment", vc.tracePrefix))
 		defer span.End()
 	}
 
-	c.tracePrefix = getEnv("TRACE_PREFIX", "vault")
-	if c.tracePrefix == "" {
+	vc.tracePrefix = getEnv("TRACE_PREFIX", "vault")
+	if vc.tracePrefix == "" {
 		return errors.New("Error occurred getting TRACE_PREFIX variable from environment")
 	}
 
-	c.vaultRole = getEnv("VAULT_ROLE", "")
-	if c.vaultRole == "" {
+	vc.vaultRole = getEnv("VAULT_ROLE", "")
+	if vc.vaultRole == "" {
 		return errors.New("You need to set the VAULT_ROLE environment variable")
 	}
 
 	// google injects this env var automatically in gcp environments
-	c.project = getEnv("GCLOUD_PROJECT", "")
-	if c.project == "" {
+	vc.project = getEnv("GCLOUD_PROJECT", "")
+	if vc.project == "" {
 		return errors.New("You need to set the GCLOUD_PROJECT environment variable")
 	}
 
 	// google injects this env var automatically in gcp environments
-	c.serviceAccount = getEnv("FUNCTION_IDENTITY", "")
-	if c.serviceAccount == "" {
+	vc.serviceAccount = getEnv("FUNCTION_IDENTITY", "")
+	if vc.serviceAccount == "" {
 		return errors.New("You need to set the FUNCTION_IDENTITY environment variable")
 	}
 
-	log.Info(fmt.Sprintf("TRACE_PREFIX=%s, VAULT_ROLE=%s, GCLOUD_PROJECT=%s, FUNCTION_IDENTITY=%s", c.tracePrefix, c.vaultRole, c.project, c.serviceAccount))
+	log.Info(fmt.Sprintf("TRACE_PREFIX=%s, VAULT_ROLE=%s, GCLOUD_PROJECT=%s, FUNCTION_IDENTITY=%s", vc.tracePrefix, vc.vaultRole, vc.project, vc.serviceAccount))
 
 	return nil
 }
@@ -69,30 +69,30 @@ func (c *vaultClient) loadVaultEnvironment() error {
 // initClient takes context and a vault role and returns an initialized Vault
 // client using the value in the "VAULT_ADDR" env var.
 // It will exit the process if it fails to initialize.
-func (c *vaultClient) initClient() (err error) {
-	if c.traceEnabled {
+func (vc *vaultClient) initClient() (err error) {
+	if vc.traceEnabled {
 		var span *trace.Span
-		c.ctx, span = trace.StartSpan(c.ctx, fmt.Sprintf("%s/initClient", c.tracePrefix))
+		vc.ctx, span = trace.StartSpan(vc.ctx, fmt.Sprintf("%s/initClient", vc.tracePrefix))
 		defer span.End()
 	}
 
-	vaultAddr, err := getEncrEnvVar(c.ctx, "VAULT_ADDR")
+	vaultAddr, err := getEncrEnvVar(vc.ctx, "VAULT_ADDR")
 	if err != nil {
 		return fmt.Errorf("Error getting vault address: %v", err)
 	}
 
-	c.client, err = api.NewClient(&api.Config{
+	vc.client, err = api.NewClient(&api.Config{
 		Address: vaultAddr,
 	})
 	if err != nil {
 		return fmt.Errorf("Error initializing new vault api client: %v", err)
 	}
-	token, err := getVaultToken(c)
+	token, err := getVaultToken(vc)
 	if err != nil {
 		return err
 	}
 
-	c.client.SetToken(token)
+	vc.client.SetToken(token)
 	if err != nil {
 		return fmt.Errorf("Error setting vault token: %v", err)
 	}
@@ -102,16 +102,16 @@ func (c *vaultClient) initClient() (err error) {
 
 // getSecretFromVault takes a vault client, key name, and data name, and returns the
 // value returned from vault as a string.
-func (c *vaultClient) getSecretFromVault(secretName string) (map[string]string, error) {
-	if c.traceEnabled {
+func (vc *vaultClient) getSecretFromVault(secretName string) (map[string]string, error) {
+	if vc.traceEnabled {
 		var span *trace.Span
-		c.ctx, span = trace.StartSpan(c.ctx, fmt.Sprintf("%s/getSecretFromVault", c.tracePrefix))
+		vc.ctx, span = trace.StartSpan(vc.ctx, fmt.Sprintf("%s/getSecretFromVault", vc.tracePrefix))
 		defer span.End()
 	}
 
 	secretMap := map[string]string{}
 
-	secretValues, err := c.client.Logical().Read(secretName)
+	secretValues, err := vc.client.Logical().Read(secretName)
 	if err != nil {
 		log.Error(fmt.Sprintf("Error reading secret from Vault: %v", err))
 		return secretMap, fmt.Errorf("error reading secret from Vault for %s", secretName)
