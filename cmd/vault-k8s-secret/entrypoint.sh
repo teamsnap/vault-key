a92e3a0f8e8206dd9c053f1377a0483052edb091
Steven Aldinger <stevenaldinger@gmail.com>
2019-12-18 13:34:35 -0500

    hack regional support into entrypoint.sh

--- cmd/vault-k8s-secret/entrypoint.sh
+++ cmd/vault-k8s-secret/entrypoint.sh
@@ -5,10 +5,16 @@ config_auth () {
     echo "PLUGIN_GOOGLE_APPLICATION_CREDENTIALS environment variable is not set." > /dev/null 2>&1
     exit 1
   else
+    location="--zone ${GCLOUD_ZONE}"
+    if [ ! -z "${GCLOUD_REGION}" ]
+    then
+      location="--region ${GCLOUD_REGION}"
+    fi
+
     gcloud auth activate-service-account --key-file "$GOOGLE_APPLICATION_CREDENTIALS" && \
     gcloud auth list && \
     gcloud config set account ${FUNCTION_IDENTITY} && \
-    gcloud container clusters get-credentials ${CLUSTER_NAME} --zone ${GCLOUD_ZONE}
+    gcloud container clusters get-credentials ${CLUSTER_NAME} ${location}
   fi
 }
 
@@ -31,11 +37,14 @@ config_region () {
   fi
 }
 
-config_zone () {
+config_location () {
   # us-east1
-  if [ -z "${GCLOUD_ZONE}" ]; then
-    echo "GCLOUD_ZONE environment variable is not set." > /dev/null 2>&1
+  if [ -z "${GCLOUD_ZONE}" && -z "${GCLOUD_REGION}" ]; then
+    echo "GCLOUD_ZONE and GCLOUD_REGION environment variables are not set. One is required." > /dev/null 2>&1
     exit 1
+  elif [ ! -z "${GCLOUD_REGION}"]
+  then
+    gcloud config set compute/region ${GCLOUD_REGION}
   else
     gcloud config set compute/zone ${GCLOUD_ZONE}
   fi
@@ -44,7 +53,7 @@ config_zone () {
 echo "Initializing gcloud..."
 config_auth && \
 config_project && \
-config_zone && \
+config_location && \
 config_region && \
 echo "Finished initializing gcloud!" && \
 ./vault-k8s-secret
