abf9e660f935162282936c4df763eaef3569fb12
Paul Hanyzewski <hanyzewski@gmail.com>
2021-12-06 15:10:25 -0700

    feat: check for all required environment config before calling logger.fatal

--- cmd/vault-staging-k8s-secret/config.go
+++ cmd/vault-staging-k8s-secret/config.go
@@ -13,22 +13,30 @@ type config struct {
 }
 
 func newConfig(lgr *zap.Logger) *config {
-	defaultEngine := os.Getenv("VAULT_SECRET")
-	lgr.Debug("vault-path to the default engine", zap.String("VAULT_SECRET", defaultEngine))
-	if len(defaultEngine) < 1 {
-		lgr.Fatal("You need to set VAULT_SECRET environment variable.")
-	}
 
+	// load required config from environment
+	defaultEngine := os.Getenv("VAULT_SECRET")
+	k8sNamespace := os.Getenv("K8S_NAMESPACE")
+
+	req := []string{}
+	if len(defaultEngine) < 1 {
+		req = append(req, ("VAULT_SECRET"))
+	}
+	if len(k8sNamespace) < 1 {
+		req = append(req, "K8S_NAMESPACE")
+	}
+	if len(req) > 0 {
+		lgr.Fatal("bad configuration", zap.Strings("missing environment variables", req))
+	}
+	lgr.Debug("vault-path to the default engine", zap.String("VAULT_SECRET", defaultEngine))
+	lgr.Debug("kubernetes namespace to apply secret", zap.String("K8S_NAMESPACE", k8sNamespace))
+
+	// load optional config from environment
 	override := os.Getenv("VAULT_SECRET_OVERRIDE")
-	lgr.Debug("vault-path to an overriding engine", zap.String("VAULT_SECRET_OVERRIDE", override))
 	if len(override) < 1 {
 		lgr.Info("No override engine provided.")
-	}
-
-	k8sNamespace := os.Getenv("K8S_NAMESPACE")
-	lgr.Debug("kubernetes namespace to apply secret", zap.String("K8S_NAMESPACE", k8sNamespace))
-	if len(k8sNamespace) < 1 {
-		lgr.Fatal("You need to set K8S_NAMESPACE environment variable.")
+	} else {
+		lgr.Debug("vault-path to an overriding engine", zap.String("VAULT_SECRET_OVERRIDE", override))
 	}
 
 	return &config{
