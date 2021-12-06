a863ebf133aa64a7ca89e628c8e9ab0ae1564a33
Paul Hanyzewski <hanyzewski@gmail.com>
2021-12-06 09:30:27 -0700

    fix: only apply secrets from verified engines

--- cmd/vault-staging-k8s-secret/main.go
+++ cmd/vault-staging-k8s-secret/main.go
@@ -26,14 +26,19 @@ func run(ctx context.Context, cfg *config) {
 		}
 	}
 
-	cfg.lgr.Info("getting secrets from vault from the verified engines", zap.Strings("verified-engines", verifiedEngines))
+	cfg.lgr.Info("getting vault secrets from verified engines", zap.Strings("verified-engines", verifiedEngines))
 	secrets := map[string]map[string]string{}
 	if err := vault.GetSecrets(ctx, &secrets, verifiedEngines); err != nil {
 		cfg.lgr.Fatal("cannot get secrets from vault", zap.Error(err))
 	}
 
-	cfg.lgr.Info("creating overrides")
-	mergedSecrets := secrets[cfg.override]
+	var mergedSecrets map[string]string
+	for _, e := range verifiedEngines {
+		if e == cfg.override {
+			cfg.lgr.Info("creating overrides")
+			mergedSecrets = secrets[cfg.override]
+		}
+	}
 
 	cfg.lgr.Info("setting defaults")
 	for k, v := range secrets[cfg.defaultEngine] {
