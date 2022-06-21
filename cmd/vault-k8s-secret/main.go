609d790cdfc4fb4edf5d8380c972b6c9efc8867a
Paul Hanyzewski <hanyzewski@gmail.com>
2022-06-21 11:04:34 -0600

    fix: [NO-JIRA] do not validate secret paths

--- cmd/vault-k8s-secret/main.go
+++ cmd/vault-k8s-secret/main.go
@@ -38,37 +38,15 @@ func run(ctx context.Context, lgr *zap.Logger) error {
 	if _, err := maxprocs.Set(); err != nil {
 		return fmt.Errorf("maxprocs: %w", err)
 	}
+
 	lgr.Info("startup", zap.Int("GOMAXPROCS", runtime.GOMAXPROCS(0)))
 
 	cfg := newConfig(lgr)
 
 	secretsToApply := []string{cfg.defaultSecretPath}
-
-	var vaultSecrets []string
-	lgr.Debug("getting secrets for engine", zap.String("engine", cfg.engine))
-
-	// If a secret to apply does not exist in vault, do not attempt to apply it
-	var verifiedSecretPaths []string
-	for _, s := range secretsToApply {
-		found := false
-
-		for _, vs := range vaultSecrets {
-			if getSecret(s) == vs {
-				lgr.Debug("secret exists", zap.String("secret", s))
-				verifiedSecretPaths = append(verifiedSecretPaths, s)
-				found = true
-				break
-			}
-		}
-
-		if !found {
-			lgr.Debug("secret does not exist", zap.String("secret", s))
-		}
-	}
-
-	lgr.Info("getting vault secrets from verified secret paths", zap.Strings("verified-secret-paths", verifiedSecretPaths))
+	lgr.Info("getting vault secrets from verified secret paths", zap.Strings("verified-secret-paths", secretsToApply))
 	secrets := map[string]map[string]string{}
-	if err := vault.GetSecrets(ctx, &secrets, verifiedSecretPaths); err != nil {
+	if err := vault.GetSecrets(ctx, &secrets, secretsToApply); err != nil {
 		return fmt.Errorf("cannot get secrets from vault: %w", err)
 	}
 
